e471c841eb6ff2c029b85ca95e5619a630aa0427
asanchezsa <ao.sanchezsaavedra@student.han.nl>
2024-11-03 13:25:07 +0100

    no message

--- notebooks/9_practice/mymodels.py
+++ notebooks/9_practice/mymodels.py
@@ -5,7 +5,8 @@ import gin
 @gin.configurable
 class MyCNN(nn.Module):
     def __init__(self, num_classes: int, unit1: int, unit2: int, kernel_size: int, dropout_rate: float, maxpoolwindow: int) -> None:
-        super(MyCNN, self).__init__()
+        super().__init__()
+        
         self.convolutions = nn.Sequential(
             nn.Conv2d(1, unit1, kernel_size=kernel_size, stride=1, padding=1),
             nn.BatchNorm2d(unit1),
