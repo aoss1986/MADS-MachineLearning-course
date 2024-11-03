cec00ebe0b82262848009ff63edaa9cf15d94886
asanchezsa <ao.sanchezsaavedra@student.han.nl>
2024-11-03 15:06:41 +0100

    configured gin file

--- notebooks/9_practice/mymodels.py
+++ notebooks/9_practice/mymodels.py
@@ -4,26 +4,26 @@ import gin
 
 @gin.configurable
 class MyCNN(nn.Module):
-    def __init__(self, num_classes: int, kernel_size: int, unit1: int, unit2: int, dropout_rate: float, maxpoolwindow: int) -> None:
+    def __init__(self, num_classes: int, kernel_size: int, filter1: int, filter2: int, dropout_rate: float, maxpoolwindow: int) -> None:
         super().__init__()
 
         self.convolutions = nn.Sequential(
-            nn.Conv2d(1, unit1, kernel_size=kernel_size, stride=1, padding=1),
-            nn.BatchNorm2d(unit1),
+            nn.Conv2d(1, filter1, kernel_size=kernel_size, stride=1, padding=1),
+            nn.BatchNorm2d(filter1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=maxpoolwindow),
-            nn.Conv2d(unit1, unit2, kernel_size=kernel_size, stride=1, padding=1),
-            nn.BatchNorm2d(unit2),
+            nn.Conv2d(filter2, 32, kernel_size=kernel_size, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=maxpoolwindow),
         )
         self.dense = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(28 * 28, unit1),
+            nn.Linear(128, 64),
             nn.ReLU(),
-            nn.Linear(unit1, unit2),
+            nn.Linear(64, 32),
             nn.ReLU(),
-            nn.Linear(unit2, num_classes),
+            nn.Linear(32, num_classes),
             nn.Softmax(dim=1),
         )
 
