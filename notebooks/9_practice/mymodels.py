2f9fe0fdc7d8c0db13388ffb686367760f9084d5
asanchezsa <ao.sanchezsaavedra@student.han.nl>
2024-11-25 15:42:25 +0100

    added parameters for LSTM model

--- notebooks/9_practice/mymodels.py
+++ notebooks/9_practice/mymodels.py
@@ -90,3 +90,38 @@ class GRUModel(nn.Module):
         out, hn = self.gru(x, h0.detach())
         out = self.fc(out[:, -1, :])
         return out
+    
+
+# Creating a LSTM model
+@gin.configurable
+class LSTM_Model(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(LSTM_Model, self).__init__()
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        # Building your LSTM
+        # batch_first=True causes input/output tensors to be of shape
+        # (batch_dim, seq_dim, feature_dim)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Initialize cell state
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Index hidden state of last time step
+        out = self.fc(out[:, -1, :])
+        return out
