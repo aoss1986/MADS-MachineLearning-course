e770e9c0549c383b7a3131e2ab21c5b26409650e
asanchezsa <ao.sanchezsaavedra@student.han.nl>
2024-11-25 13:12:32 +0100

    added GRU parameters to gin file

--- notebooks/9_practice/mymodels.py
+++ notebooks/9_practice/mymodels.py
@@ -74,3 +74,19 @@ class MyCNN(nn.Module):
         x = self.layer4(x)
         x = self.dense(x)
         return x
+    
+
+@gin.configurable
+class GRUModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(GRUModel, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.layer_dim = layer_dim
+        self.gru = nn.GRU(input_dim, hidden_dim, layer_dim, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+        out, hn = self.gru(x, h0.detach())
+        out = self.fc(out[:, -1, :])
+        return out
