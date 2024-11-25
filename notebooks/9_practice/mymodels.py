8a190bee53ec3ce520a4e35b90bbb1849b0129dc
asanchezsa <ao.sanchezsaavedra@student.han.nl>
2024-11-25 16:40:26 +0100

    added LSTM-hypertuning model to tune a model by auto tuning

--- notebooks/9_practice/mymodels.py
+++ notebooks/9_practice/mymodels.py
@@ -125,3 +125,38 @@ class LSTM_Model(nn.Module):
         # Index hidden state of last time step
         out = self.fc(out[:, -1, :])
         return out
+    
+
+# LSTM model that will be used for hyperparameter tuning instead of manual tuning of aprameters
+
+class LSTM_HyperModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+        super(LSTM_HyperModel, self).__init__()
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        # Building your LSTM
+        # batch_first=True causes input/output tensors to be of shape
+        # (batch_dim, seq_dim, feature_dim)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Initialize cell state
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Index hidden state of last time step
+        out = self.fc(out[:, -1, :])
+        return out
